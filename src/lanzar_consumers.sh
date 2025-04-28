28c09c824e6832caa7178e5558e13b072abd0543
AbrilUrcuyo <aurcuyoarce@gmail.com>
2025-04-27 22:32:17 -0600

    Detalles consumidores, pruebas con 20 consumer y 100 producers

---
+++ src/lanzar_consumers.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Número de consumidores que querés lanzar
+NUM_CONSUMERS=20
+
+# Ruta al ejecutable de tu consumer
+CONSUMER_EXEC=./consumer  # Cambiá el nombre si tu ejecutable se llama diferente
+
+for i in $(seq 1 $NUM_CONSUMERS)
+do
+    echo "Lanzando Consumer $i"
+    $CONSUMER_EXEC &
+done
+
+echo "Todos los Consumers fueron lanzados."
